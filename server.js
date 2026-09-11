e2c48d4167bc869c7276e3e3e5d371df14529339
Panupong Sangkatin <heddylemillion@gmail.com>
2026-09-11 09:42:29 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Portfolio server running at http://localhost:${port}`);
+});
