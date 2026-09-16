f6dea7c79942cf1f44166b2ba15bd3024f863992
Panupong Sangkatin <heddylemillion@gmail.com>
2026-09-16 10:01:12 +0700

    update3

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
